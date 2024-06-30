0d1e642cebc02bebf9d1b9f242aa64393d80f855
Hayato Tenryuu <hayatotenryuu@gmail.com>
2024-06-30 15:59:09 -0500

    Added app caller function to include a sumamry and such.

---
+++ main.m
@@ -0,0 +1,13 @@
+%% App to Solve Sudoku Puzzles
+
+%{
+Summary:
+
+This is a MATLAB version of the same project in C# and Qt/C++.
+And to be honest, MATLAB apps have such limited doability, I 
+really prefer the others compared to this one. I can't control
+any of the good parts of this app...
+
+%}
+
+run app1.mlapp;     % I have all the setup and such within the app code file, so this is enough.
